7651445e26c307f3d7dca808b3cf72552d235ea2
Antonio Matheus <matheus69969@gmail.com>
2025-02-24 16:03:41 -0300

    Adicionando Arquivos JS

--- 11.js
+++ 11.js
@@ -1,23 +1,22 @@
-let impar=[]
+let impa=[]
 let par=[]
-let numeros=[1,2,3,4,5,6,7,8]
-function numeropar(){
-    for(let i=0; i< numeros.length;i++){
-        if(numeros[i]%2===0){
-            par.push(numeros[i]);
-        }
-    }
-}
-    
+let numeros=[1,2,3,4,5,6,7,8,]
+
 function numeroimpar(){
     for(let i=0; i<numeros.length; i++){
         if(numeros[i]%2===1){
-            impar.push(numeros[i])
+            impa.push(numeros[i])
         }
     }
 }
+function numerospar(){
+    for(let i=0; i<numeros.length; i++){
+        if(numeros[i]%2===0){
+            par.push(numeros[i])
+        }
+    }
+}
+numerospar()
 numeroimpar()
-numeropar()
-
-console.log(`Números par ${par}`)
-console.log(`Números impar ${impar}`)
+console.log(impa)
+console.log(par)
---
+++ 12.js
@@ -0,0 +1,9 @@
+const numero=prompt('Digite um número: ')
+
+let contador=0
+while (contador<=numero) {
+if(contador%2===0){
+    console.log(contador)
+}
+    contador++
+}
