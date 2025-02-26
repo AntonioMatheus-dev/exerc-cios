ffecde7d74eb073c8d8d51a4eff1c44ac1e0ee56
Antonio Matheus <matheus69969@gmail.com>
2025-02-26 13:28:14 -0300

    Adicionando Arquivos JS

---
+++ 17.js
@@ -0,0 +1,14 @@
+let [A, B, C]=prompt(': ').split(' ').map(Number);
+const pi=3.14159;
+
+const triangulo=(A * C)/2;
+const circulo=pi*Math.pow(C, 2);
+const trapezio=((A+B)*C)/2;
+const quadrado=B*B;
+const retangulo=A*B;
+
+console.log(`TRIANGULO: ${triangulo.toFixed(3)}`);
+console.log(`CIRCULO: ${circulo.toFixed(3)}`);
+console.log(`TRAPEZIO: ${trapezio.toFixed(3)}`);
+console.log(`QUADRADO: ${quadrado.toFixed(3)}`);
+console.log(`RETANGULO: ${retangulo.toFixed(3)}`);
